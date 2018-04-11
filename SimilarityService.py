6658cead3600c31273d6a9eba9d67a854d329d2a
Shantanu Mukherjee <shantanu27.bits@gmail.com>
2018-04-10 23:27:29 -0400

    Fixing similarity service

--- SimilarityService.py
+++ SimilarityService.py
@@ -1,20 +1,27 @@
-
 from gensim.models import KeyedVectors
-class SimilarityService
+
+
+class SimilarityService:
 
     def _init_(self):
         filename = 'GoogleNews-vectors-negative300.bin'
         self.model = KeyedVectors.load_word2vec_format(filename, binary=True)
 
-    @static
-    def get_word_vector(word):
+    @classmethod
+    def get_word_vector(self, word):
         return self.model[word]
 
-
-    @static
-    def get_similar_word_embeddings(positive_words, negative_words, topn = 1):
+    @classmethod
+    def get_similar_word_embeddings(self, positive_words, negative_words, topn = 1):
         return self.model.most_similar(positive=positive_words, negative=negative_words, topn=topn)
 
-    @static
-    def get_word_to_word_similarity(first_word, second_word):
+    @classmethod
+    def get_word_to_word_similarity(self, first_word, second_word):
         return self.model.wv.similarity(first_word, second_word)
+
+    @classmethod
+    def get_sentence_similarity_matrix(self, s1, s2):
+        s1_words = s1.split()
+        s2_words = s2.split()
+        return k[[self.get_word_to_word_similarity(w1, w2) for w1 in s1_words] for w2 in s2_words]
+
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask.ext.api import status
-import SimilarityService from SimilarityService
+from flask import Response
+from requests import request
+from SimilarityService import SimilarityService
 
 app = Flask(__name__)
 
@@ -8,43 +9,51 @@ app = Flask(__name__)
 def get_word_embeddings():
     try:
         word = request.args.get('word')
-        return SimilarityService.get_word_vector(word)
+        return Response(SimilarityService.get_word_vector(word),
+                        status=200,
+                        content_type='application/json')
     except KeyError:
-        contect = { 'message' : 'INVALID PARAMS' }
-        return content, status.HTTP_400_BAD_REQUEST
+        content = {'message': 'INVALID PARAMS'}
+        return Response(content, status=400, content_type='application/json')
+
 
 @app.route('/getSimilarWordEmbeddings')
 def get_similar_word_embeddings():
     try:
         positive_words = request.args.get('positive_words').split(',')
-        negative_words=[]
+        negative_words = []
         topn = 1
         if request.args.get('negative_words') is not None:
             negative_words = request.args.get('negative_words').split(',')
         if request.args.get('topn') is not None:
             topn = request.args.get('topn')
-        return SimilarityService.get_similar_word_embeddings(positive_words, negative_words , topn)
+        return Response(SimilarityService.get_similar_word_embeddings(positive_words, negative_words, topn),
+                        status=200,
+                        content_type='application/json')
 
     except KeyError:
-        contect = { 'message' : 'INVALID PARAMS' }
-        return content, status.HTTP_400_BAD_REQUEST
+        content = {'message': 'INVALID PARAMS'}
+        return Response(content, status=400, content_type='application/json')
 
 
 @app.route('/getWordToWordSimilarity')
-def getWordToWordSimilarity():
+def get_word_to_word_similarity():
     try:
         word1 = request.args.get('word1')
         word2 = request.args.get('word2')
-        return SimilarityService.get_similar_word_embeddings(word1,word2)
+        return Response(SimilarityService.get_similar_word_embeddings(word1,word2),
+                        status=200,
+                        content_type='application/json')
 
     except KeyError:
-        contect = { 'message' : 'INVALID PARAMS' }
-        return content, status.HTTP_400_BAD_REQUEST
+        content = {'message': 'INVALID PARAMS'}
+        return Response(content, status=400, content_type='application/json')
 
 
 @app.route('/getSentenceSimilarityMatrix')
-def getSentenceSimilarityMatrix():
-    return "Sentence"
+def get_sentence_similarity_():
+    return SimilarityService.get_sentence_similarity_matrix(s1, s2)
+
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True)
