3fd0368e67b429f513b25378ab198c84c45145bc
Shantanu Mukherjee <shantanu27.bits@gmail.com>
2018-04-24 13:58:40 -0400

    Fixing logger

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/getWordVector')
 def get_word_embeddings():
     try:
-        app.logger.info(request.method, request.path, request.args)
+        # app.logger.info(request.method, request.path, request.args)
         word = request.args.get('word')
         return Response(SimilarityService.get_word_vector(word),
                         status=200,
@@ -26,7 +26,7 @@ def get_word_embeddings():
 @app.route('/getSimilarWordEmbeddings')
 def get_similar_word_embeddings():
     try:
-        app.logger.info(request.method, request.path, request.args)
+        # app.logger.info(request.method, request.path, request.args)
         positive_words = request.args.get('positive_words').split(',')
         negative_words = []
         topn = 1
@@ -46,7 +46,7 @@ def get_similar_word_embeddings():
 @app.route('/getWordToWordSimilarity')
 def get_word_to_word_similarity():
     try:
-        app.logger.info(request.method, request.path, request.args)
+        # app.logger.info(request.method, request.path, request.args)
         word1 = request.args.get('word1')
         word2 = request.args.get('word2')
         return Response(SimilarityService.get_word_to_word_similarity(word1, word2),
@@ -61,7 +61,7 @@ def get_word_to_word_similarity():
 @app.route('/getSentenceSimilarityMatrix')
 def get_sentence_similarity():
     try:
-        app.logger.info(request.method, request.path, request.args)
+        # app.logger.info(request.method, request.path, request.args)
         s1 = request.args.get('s1')
         s2 = request.args.get('s2')
         return Response(SimilarityService.get_sentence_similarity_matrix(s1, s2),
@@ -72,14 +72,20 @@ def get_sentence_similarity():
         return Response(content, status=400, content_type='application/json')
 
 
-if __name__ == '__main__':
-    SimilarityService()
+@app.after_request
+def after_request(response):
+    app.logger.info(request.method, request.path, request.args)
+    return response
 
+
+if __name__ == '__main__':
     formatter = logging.Formatter(
-        "[%(asctime)s] {%(pathname)s} %(levelname)s - %(message)s]"
+        "[%(levelname)s - %(message)s]"
     )
     logFileName = 'logs/{}.log'.format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d_%H_%M'))
     handler = RotatingFileHandler(logFileName, maxBytes=10000000)
-    handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
+    handler.formatter = formatter
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.DEBUG)
+    log.addHandler(handler)
     app.run(threaded=True)
