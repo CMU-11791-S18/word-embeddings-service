161614540c95e0b303ecbdac3142aef3fd8de0ee
Shantanu Mukherjee <shantanu27.bits@gmail.com>
2018-04-25 18:31:11 -0400

    Wrapped response in string

--- SimilarityService.py
+++ SimilarityService.py
@@ -20,11 +20,11 @@ class SimilarityService:
 
     @classmethod
     def get_word_to_word_similarity(self, first_word, second_word):
-        return str(self.model.wv.similarity(first_word, second_word))
+        return self.model.wv.similarity(first_word, second_word)
 
     @classmethod
     def get_sentence_similarity_matrix(self, s1, s2):
         m, n = len(s1), len(s2)
         print(m, n)
-        W_s1_s2 = [[str(SimilarityService.get_word_to_word_similarity(s1[x], s2[y])) for y in range(n)] for x in range(m)]
+        W_s1_s2 = [[SimilarityService.get_word_to_word_similarity(s1[x], s2[y]) for y in range(n)] for x in range(m)]
         return json.dumps(W_s1_s2)
