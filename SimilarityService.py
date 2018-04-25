bc2eea3a7ef0008a3532effb41aa3873e2c61325
Shantanu Mukherjee <shantanu27.bits@gmail.com>
2018-04-25 18:56:46 -0400

    Fixed exception

--- SimilarityService.py
+++ SimilarityService.py
@@ -29,7 +29,5 @@ class SimilarityService:
     @classmethod
     def get_sentence_similarity_matrix(self, s1, s2):
         m, n = len(s1), len(s2)
-
         W_s1_s2 = [[SimilarityService.get_word_to_word_similarity(s1[x], s2[y]) for y in range(n)] for x in range(m)]
-        print(s1, s2)
         return json.dumps(W_s1_s2)
