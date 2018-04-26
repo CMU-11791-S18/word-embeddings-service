4cfa5e712073ae46646a3b0fa2e1698f26cd419f
Sangam Kumar <sangamkumar91@gmail.com>
2018-04-26 02:25:15 -0400

    Exception handlele in similarSentencematrix

--- SimilarityService.py
+++ SimilarityService.py
@@ -30,6 +30,9 @@ class SimilarityService:
 
     @classmethod
     def get_sentence_similarity_matrix(self, s1, s2):
-        m, n = len(s1), len(s2)
-        W_s1_s2 = [[SimilarityService.get_word_to_word_similarity(s1[x], s2[y]) for y in range(n)] for x in range(m)]
-        return json.dumps(W_s1_s2)
+        try:
+            m, n = len(s1), len(s2)
+            W_s1_s2 = [[SimilarityService.get_word_to_word_similarity(s1[x], s2[y]) for y in range(n)] for x in range(m)]
+            return json.dumps(W_s1_s2)
+        except:
+            return json.dumps([])
